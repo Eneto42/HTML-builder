f450515cfef27c0dc47ec0c5d6566c52606ec9ea
Eneto <omgeneto@gmail.com>
2022-05-24 10:32:07 +0300

    feat: add 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+const folder = path.join(__dirname, 'files')
+const pathCopy = path.join(__dirname, 'files-copy')
 
+fs.mkdir(pathCopy, { recursive: true }, err => { if (err) throw err })
+fs.readdir(folder, (error, files) => {
+  if (error) throw error
+  files.forEach(f => {
+    fs.stat(`${folder}/${f}`, (error, file) => {
+      if (file.isFile()) {
+        const folderFile = path.join(folder, f)
+        const copyFolder = path.join(pathCopy, f)
+        fs.copyFile(folderFile, copyFolder, error => { if (error) throw error })
+        }
+    })
+})
+console.log('files copied!')
+})
